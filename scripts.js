14a394e8c2a0671a032f204a4871779c4c1164b7
Harsh Pandhe <pandhemihir@gmail.com>
2024-07-10 18:45:25 +0530

    Final Version

--- scripts.js
+++ scripts.js
@@ -1,12 +1,23 @@
-const quotes = [
-    "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
-    "The way to get started is to quit talking and begin doing. - Walt Disney",
-    "Your time is limited, so don't waste it living someone else's life. - Steve Jobs",
-    "If life were predictable it would cease to be life, and be without flavor. - Eleanor Roosevelt",
-    "If you look at what you have in life, you'll always have more. - Oprah Winfrey"
-];
+
+let quotes = [];
+
+async function fetchQuotes() {
+    const response = await fetch('https://type.fit/api/quotes');
+    quotes = await response.json();
+    generateQuote();
+}
 
 function generateQuote() {
+    if (quotes.length === 0) {
+        alert('No quotes available. Please try again later.');
+        return;
+    }
     const randomIndex = Math.floor(Math.random() * quotes.length);
-    document.getElementById('quote').textContent = quotes[randomIndex];
+    const quote = quotes[randomIndex];
+    const quoteText = quote.text;
+    document.getElementById('quote').textContent = `${quoteText}`;
 }
+
+document.getElementById('generateBtn').addEventListener('click', generateQuote);
+
+fetchQuotes();
